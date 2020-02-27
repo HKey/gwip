82091338a87805e50044df667fdf295abb8ec028
HKey <s06139@gmail.com>
2020-02-27 16:51:23 +0900

    Update demo screencast

--- screencast/gwip-demo.sh
+++ screencast/gwip-demo.sh
@@ -2,18 +2,25 @@
 
 set -e
 
-function call() {
+function demo() {
   notify-send "$*"
   $*
   sleep 1
 }
 
-call gwip move --grid=2x1 --place=0,0 # top
-call gwip move --grid=2x1 --place=1,0 # bottom
-call gwip move --grid=1x2 --place=0,0 # left
-call gwip move --grid=1x2 --place=0,1 # right
-call gwip move --grid=2x2 --place=0,0 # top left
-call gwip move --grid=2x2 --place=0,1 # top right
-call gwip move --grid=2x2 --place=1,0 # bottom left
-call gwip move --grid=2x2 --place=1,1 # bottom right
-call gwip move --grid=1x1 --place=0,0 # center
+# move only
+demo gwip move --grid=2x1 --place=0,0 # top
+demo gwip move --grid=2x1 --place=1,0 # bottom
+demo gwip move --grid=1x2 --place=0,0 # left
+demo gwip move --grid=1x2 --place=0,1 # right
+demo gwip move --grid=2x2 --place=0,0 # top left
+demo gwip move --grid=2x2 --place=0,1 # top right
+demo gwip move --grid=2x2 --place=1,0 # bottom left
+demo gwip move --grid=2x2 --place=1,1 # bottom right
+demo gwip move --grid=1x1 --place=0,0 # center
+
+# move and fill
+demo gwip move --grid=1x2 --place=0,0 --fill --gap=2%x2% # left
+demo gwip move --grid=1x2 --place=0,1 --fill --gap=2%x2% # right
+demo gwip move --grid=1x1 --place=0,0 --fill --gap=2%x2% # center wide
+demo gwip move --grid=1x1 --place=0,0 --fill --gap=26%x2% # center tall
